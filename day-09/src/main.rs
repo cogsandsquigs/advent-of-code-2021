6cbfc9546a129d70e5d33636f8ffbaad16f8a167
Ian Pratt <ianjdpratt@gmail.com>
2022-12-02 16:53:43 +0000

    changes

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -16,24 +16,10 @@ fn main() -> Result<()> {
     for (y, row) in heightmap.iter().enumerate() {
         for (x, height) in row.iter().enumerate() {
             // The surrounding heights
-            let mut surrounds: Vec<u32> = vec![];
-
-            // If we aren't idx 0 for x, we can get the prev. digit
-            if x > 0 {
-                surrounds.push(row[x - 1]);
-            }
-            // If we aren't idx 0 for x, we can get the digit above us
-            if y > 0 {
-                surrounds.push(heightmap[y - 1][x]);
-            }
-            // If we aren't past row length, we can get next digit
-            if x < row.len() - 1 {
-                surrounds.push(row[x + 1]);
-            }
-            // If we aren't past height length, we can get below digit
-            if y < heightmap.len() - 1 {
-                surrounds.push(heightmap[y + 1][x]);
-            }
+            let surrounds: Vec<u32> = neighbors(&heightmap, (x, y))
+                .into_iter()
+                .map(|(x, y)| heightmap[y][x])
+                .collect();
 
             if surrounds.iter().all(|surround| height < surround) {
                 lowests.push(*height);
@@ -46,5 +32,66 @@ fn main() -> Result<()> {
         lowests.iter().fold(0, |acc, x| acc + x + 1)
     );
 
+    // println!("{:?}", bfs_basin(&heightmap, (0, 0), &vec![]));
+
     Ok(())
+}
+
+/// BFS to find the size and points of and in the basin, given an individual starting point.
+/// Note that because all basins are separate, we only need to expect the starting point to
+/// not be in any explored basin/point before.
+fn bfs_basin(
+    heightmap: &Vec<Vec<u32>>,
+    start_pt: (usize, usize),
+    explored: &Vec<(usize, usize)>,
+) -> (usize, Vec<(usize, usize)>) {
+    // List of points to explore
+    let mut to_explore: Vec<(usize, usize)> = vec![start_pt];
+
+    // List of points we have explored
+    let mut explored: Vec<(usize, usize)> = explored.to_vec();
+
+    while !to_explore.is_empty() {
+        let point = to_explore.pop().unwrap();
+
+        // Get its neighbors
+        let neighbors = neighbors(heightmap, point);
+
+        for neighbor in neighbors {
+            if !explored.contains(&neighbor) {
+                to_explore.push(neighbor);
+            }
+        }
+
+        explored.push(point);
+
+        println!("{:?}", point);
+    }
+
+    (explored.len(), explored)
+}
+
+/// Get the surrounding points in the heightmap
+fn neighbors(heightmap: &Vec<Vec<u32>>, start_pt: (usize, usize)) -> Vec<(usize, usize)> {
+    let (x, y) = start_pt;
+    let mut surrounds: Vec<(usize, usize)> = vec![];
+
+    // If we aren't idx 0 for x, we can get the prev. digit
+    if x > 0 {
+        surrounds.push((x - 1, y));
+    }
+    // If we aren't idx 0 for x, we can get the digit above us
+    if y > 0 {
+        surrounds.push((x, y - 1));
+    }
+    // If we aren't past row length, we can get next digit
+    if x < heightmap[0].len() - 1 {
+        surrounds.push((x + 1, y));
+    }
+    // If we aren't past height length, we can get below digit
+    if y < heightmap.len() - 1 {
+        surrounds.push((x, y + 1));
+    }
+
+    surrounds
 }
