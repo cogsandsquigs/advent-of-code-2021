198e7b9806963fcadb6d53f623193e864614dc49
Ian Pratt <ianjdpratt@gmail.com>
2022-12-02 21:06:26 -0600

    complete day 9

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # advent-of-code-2021
-My re-doing of the 2021 advent of code (in rust)
+
+My re-doing of the 2021 advent of code (in rust). Note that I'm not going to be doing the puzzles in order, but rather in the order that I feel like doing them. Also, some days may be skipped if I don't feel like doing them, or if I've done them before (in a different language).
--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -12,103 +12,71 @@ fn main() -> Result<()> {
         .collect();
 
     // Collect all the lowest pts
-    let mut lowests: Vec<u32> = vec![];
+    let mut lowests: Vec<(usize, usize)> = vec![];
 
     for (y, row) in heightmap.iter().enumerate() {
         for (x, height) in row.iter().enumerate() {
             // The surrounding heights
-            let surrounds: Vec<u32> = neighbors(&heightmap, (x, y))
-                .into_iter()
-                .map(|(x, y)| heightmap[y][x])
-                .collect();
+            let surrounds: Vec<(usize, usize)> = neighbors(&heightmap, (x, y));
 
-            if surrounds.iter().all(|surround| height < surround) {
-                lowests.push(*height);
+            if surrounds
+                .iter()
+                .all(|surround| height < &heightmap[surround.1][surround.0])
+            {
+                lowests.push((x, y));
             }
         }
     }
 
     println!(
         "Part 1 answer: {}",
-        lowests.iter().fold(0, |acc, x| acc + x + 1)
+        lowests
+            .iter()
+            .fold(0, |acc, pt| acc + heightmap[pt.1][pt.0] + 1)
     );
 
-    let mut explored = HashSet::new();
-    let mut top_3: Vec<usize> = vec![];
+    let mut basins: Vec<usize> = vec![];
 
-    while let Some(start_pt) = next_pt(&heightmap, &explored) {
-        let size = bfs_basin(&heightmap, start_pt, &mut explored);
-
-        top_3.push(size);
-        top_3.sort();
-        top_3.reverse();
-        top_3 = top_3.into_iter().take(3).collect();
-
-        println!("{:?}", top_3);
+    // Use the lowest points to find the basins
+    for start_pt in lowests {
+        let size = bfs_basin(&heightmap, start_pt);
+        basins.push(size);
     }
+
+    // Sort the top 3
+    basins.sort();
+    basins.reverse();
 
     println!(
         "Part 2 answer: {}",
-        top_3.into_iter().fold(1, |acc, x| acc * x)
+        basins.into_iter().take(3).fold(1, |acc, x| acc * x)
     );
 
     Ok(())
 }
 
-/// Gets the next point to explore
-fn next_pt(
-    heightmap: &Vec<Vec<u32>>,
-    explored: &HashSet<(usize, usize)>,
-) -> Option<(usize, usize)> {
-    if explored.is_empty() {
-        return Some((0, 0));
-    }
-
-    for point in explored {
-        let neighbors = neighbors(heightmap, *point);
-
-        for neighbor in neighbors {
-            if !explored.contains(&neighbor) {
-                return Some(neighbor);
-            }
-        }
-    }
-
-    None
-}
-
 /// BFS to find the size and points of and in the basin, given an individual starting point.
 /// Note that because all basins are separate, we only need to expect the starting point to
 /// not be in any explored basin/point before. Updates `explored` with the new explored values
-fn bfs_basin(
-    heightmap: &Vec<Vec<u32>>,
-    start_pt: (usize, usize),
-    explored: &mut HashSet<(usize, usize)>,
-) -> usize {
-    // Size of basin
+fn bfs_basin(heightmap: &Vec<Vec<u32>>, start_pt: (usize, usize)) -> usize {
+    let mut explored: HashSet<(usize, usize)> = HashSet::new();
+    let mut explore_queue: Vec<(usize, usize)> = vec![start_pt];
     let mut size = 0;
 
-    // List of points to explore
-    let mut to_explore: Vec<(usize, usize)> = vec![start_pt];
+    while !explore_queue.is_empty() {
+        let pt = explore_queue.pop().unwrap();
 
-    while !to_explore.is_empty() {
-        let point = to_explore.pop().unwrap();
-        explored.insert(point);
-
-        // If it's 9 (or greater, should be impossible), then we know we can't explore
-        // further than that.
-        if heightmap[point.1][point.0] >= 9 {
+        if explored.contains(&pt) || heightmap[pt.1][pt.0] == 9 {
             continue;
         }
 
+        explored.insert(pt);
         size += 1;
 
-        // Get its neighbors
-        let neighbors = neighbors(heightmap, point);
-
-        for neighbor in neighbors {
+        // Add all the neighbors to the queue
+        for neighbor in neighbors(heightmap, pt) {
             if !explored.contains(&neighbor) {
-                to_explore.push(neighbor);
+                explore_queue.push(neighbor);
             }
         }
     }
@@ -119,24 +87,24 @@ fn bfs_basin(
 /// Get the surrounding points in the heightmap
 fn neighbors(heightmap: &Vec<Vec<u32>>, start_pt: (usize, usize)) -> Vec<(usize, usize)> {
     let (x, y) = start_pt;
-    let mut surrounds: Vec<(usize, usize)> = vec![];
+    let mut neighbors: Vec<(usize, usize)> = vec![];
 
     // If we aren't idx 0 for x, we can get the prev. digit
     if x > 0 {
-        surrounds.push((x - 1, y));
+        neighbors.push((x - 1, y));
     }
     // If we aren't idx 0 for x, we can get the digit above us
     if y > 0 {
-        surrounds.push((x, y - 1));
+        neighbors.push((x, y - 1));
     }
     // If we aren't past row length, we can get next digit
     if x < heightmap[0].len() - 1 {
-        surrounds.push((x + 1, y));
+        neighbors.push((x + 1, y));
     }
     // If we aren't past height length, we can get below digit
     if y < heightmap.len() - 1 {
-        surrounds.push((x, y + 1));
+        neighbors.push((x, y + 1));
     }
 
-    surrounds
+    neighbors
 }
