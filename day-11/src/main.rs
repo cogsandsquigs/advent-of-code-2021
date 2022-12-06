cd89a54922aa305bca741c0bbfb9e503e11304dc
Ian Pratt <ianjdpratt@gmail.com>
2022-12-06 20:13:33 +0000

    changes

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 use utils::files::read_file_string;
 
 fn main() -> Result<()> {
-    let input = read_file_string("day-11/input.txt")?;
+    let input = read_file_string("day-11/input.test.txt")?;
 
     println!("Part 1 answer: {}", part_1(&input));
 
@@ -19,86 +19,70 @@ fn part_2(input: &String) -> usize {
 }
 
 fn part_1(input: &String) -> usize {
-    let octopi: Vec<Vec<Rc<RefCell<u32>>>> = input
+    let octopi: Vec<Vec<u32>> = input
         .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| Rc::new(RefCell::new(c.to_digit(10).unwrap())))
-                .collect()
-        })
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
-
-    let mut num_flashes = 0;
-
-    convolve_in_place(&octopi, |center, neighbors| {
-        // Increment center
-        center.replace_with(|&mut x| x + 1);
-
-        if *center.borrow() > 9 {
-            num_flashes += 1;
-
-            neighbors.into_iter().map(|loc| {
-                loc.replace_with(|&mut x| x + 1);
-                loc
-            });
-
-            center.replace(0);
-        }
-    });
 
     println!("{:?}", octopi);
 
     todo!();
 }
 
-fn convolve_in_place<T: Clone>(
-    matrix: &Vec<Vec<Rc<RefCell<T>>>>,
-    mut kernel: impl FnMut(Rc<RefCell<T>>, Vec<Rc<RefCell<T>>>),
+fn convolve(
+    matrix: &Vec<Vec<Rc<RefCell<u32>>>>,
+    mut kernel: impl FnMut(&Vec<Vec<Rc<RefCell<u32>>>>, (usize, usize), Vec<(usize, usize)>),
 ) {
-    let height = matrix.len();
-    let width = matrix[0].len();
-    for y in 0..height {
-        for x in 0..width {
-            let center = matrix[y][x].clone();
-            let neighbors: Vec<Rc<RefCell<T>>> = neighbors(&matrix, x, y);
-
-            kernel(center, neighbors)
+    for y in 0..matrix.len() {
+        for x in 0..matrix[0].len() {
+            convolve_at(matrix, &mut kernel, (x, y))
         }
     }
 }
 
-fn neighbors<T>(matrix: &Vec<Vec<Rc<RefCell<T>>>>, x: usize, y: usize) -> Vec<Rc<RefCell<T>>> {
+fn convolve_at(
+    matrix: &Vec<Vec<Rc<RefCell<u32>>>>,
+    kernel: &mut impl FnMut(&Vec<Vec<Rc<RefCell<u32>>>>, (usize, usize), Vec<(usize, usize)>),
+    point: (usize, usize),
+) {
+    let neighbors = neighbors(&matrix, point);
+
+    kernel(matrix, point, neighbors);
+}
+
+fn neighbors<T>(matrix: &Vec<Vec<T>>, point: (usize, usize)) -> Vec<(usize, usize)> {
+    let (x, y) = point;
     let height = matrix.len() - 1;
     let width = matrix[0].len() - 1;
-    let mut neighbors: Vec<Rc<RefCell<T>>> = vec![];
+    let mut neighbors: Vec<(usize, usize)> = vec![];
 
     if x > 0 {
         if y > 0 {
-            neighbors.push(matrix[y - 1][x - 1].clone());
+            neighbors.push((x - 1, y - 1));
         }
 
         if y < height {
-            neighbors.push(matrix[y + 1][x - 1].clone());
+            neighbors.push((x - 1, y + 1));
         }
 
-        neighbors.push(matrix[y][x - 1].clone());
+        neighbors.push((x - 1, y));
     }
     if y > 0 {
         if x < width {
-            neighbors.push(matrix[y - 1][x + 1].clone());
+            neighbors.push((x + 1, y - 1));
         }
 
-        neighbors.push(matrix[y - 1][y].clone());
+        neighbors.push((x, y - 1));
     }
     if x < width {
         if y < height {
-            neighbors.push(matrix[y + 1][x + 1].clone());
+            neighbors.push((x + 1, y + 1));
         }
 
-        neighbors.push(matrix[y][x + 1].clone());
+        neighbors.push((x + 1, y));
     }
     if y < height {
-        neighbors.push(matrix[y + 1][x].clone());
+        neighbors.push((x, y + 1));
     }
 
     neighbors
